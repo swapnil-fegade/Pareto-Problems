27c2f9e6b000867cdb0f21a8099bad6edc8295d9
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2025-06-30 12:32:25 +0530

    Updated with optimized sol longest-consecutive-sequence.java

--- longest-consecutive-sequence.java
+++ longest-consecutive-sequence.java
@@ -1,3 +1,4 @@
+// My sol
 class Solution {
     public int longestConsecutive(int[] nums) {
         if(nums.length == 0) return 0;
@@ -20,5 +21,31 @@ class Solution {
         }
 
         return maxLen+1;
+    }
+}
+
+// Optimised sol
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> st = new HashSet<>();
+        for (int num : nums) {
+            st.add(num);
+        }
+
+        int maxLen = 0;
+
+        for (int num : st) {
+            if (!st.contains(num - 1)) {
+                int length = 1;
+
+                while (st.contains(num + length)) {
+                    length++;
+                }
+
+                maxLen = Math.max(maxLen, length);
+            }
+        }
+
+        return maxLen;        
     }
 }
