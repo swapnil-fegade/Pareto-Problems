87fcfba9d7522a7fbd255203905fa5ac49314c72
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2025-07-06 17:54:07 +0530

    Sol merge-two-sorted-lists.java

---
+++ merge-two-sorted-lists.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Interatively 
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Dummy node to act as the starting point of the merged list
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        // Merge the two lists
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // At least one of the lists is now null, attach the remaining list
+        current.next = (list1 != null) ? list1 : list2;
+
+        // Return the merged list (skipping the dummy node)
+        return dummy.next;
+    }
+}
+
+// Using recurrsion
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if(list1!=null && list2!=null){
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
+            }
+            else{
+                list2.next=mergeTwoLists(list1,list2.next);
+                return list2;
+        }
+        }
+        if(list1==null)
+            return list2;
+        return list1;
+    }
+}
