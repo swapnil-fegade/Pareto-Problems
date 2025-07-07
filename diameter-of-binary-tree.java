fc4a8b3e04cae4fe9d43633c6fbecf8b7aeec78e
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2025-07-07 23:53:30 +0530

    Sol diameter-of-binary-tree.java

---
+++ diameter-of-binary-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        dia(root);
+        return res;
+    }
+
+    int dia(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = dia(root.left);
+        int right = dia(root.right);
+
+        res = Math.max(res, left+right);
+
+        return 1 + Math.max(left, right);
+    }
+}
