1af4159e67eca9b591d286a73bca2ccbe6abd9c4
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2025-07-08 00:41:23 +0530

    sol balanced-binary-tree.java

---
+++ balanced-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        
+        if (root == null)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+    
+	public int Height(TreeNode root) {
+        
+		if (root == null)  return 0;
+        
+		int left = Height(root.left);
+        int right = Height(root.right);
+
+        // In case of left subtree or right subtree unbalanced, return -1...
+		if (left == -1 || right == -1)  return -1;
+        // If their heights differ by more than ‘1’, return -1...
+        if (Math.abs(left - right) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+		return Math.max(left, right) + 1;
+    }
+}
