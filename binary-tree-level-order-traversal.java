b128343a085d08d18d4512ccb96722359c4baaf5
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2025-07-10 23:17:14 +0530

    Sol binary-tree-level-order-traversal.java

---
+++ binary-tree-level-order-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root); 
+
+        while(!queue.isEmpty()){
+            int levelsize = queue.size();
+            List<Integer> Level = new ArrayList<>();
+
+            for(int i = 0; i< levelsize; i++){
+                TreeNode node = queue.poll();
+                
+                Level.add(node.val);
+
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+
+            result.add(Level);
+        }
+        return result;
+    }
+}
